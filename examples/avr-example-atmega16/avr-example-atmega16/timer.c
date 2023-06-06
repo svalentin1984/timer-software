bab62fbe0ba57013004886ed4a775a328ecf6932
Valentin Stangaciu <valentin.stangaciu@cs.upt.ro>
2023-06-06 09:20:34 +0300

    added example project for Microchip Studio for ATMEGA16 Microcontroller.
    Signed-off-by: Valentin Stangaciu <valentin.stangaciu@cs.upt.ro>

---
+++ examples/avr-example-atmega16/avr-example-atmega16/timer.c
@@ -0,0 +1,30 @@
+/*
+ * timer.c
+ *
+ * Created: 06.06.2023 08:43:30
+ *  Author: valentin.stangaciu
+ */ 
+
+#define F_CPU 14745600UL
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "timer.h"
+#include "timer_software.h"
+
+unsigned int n = 0;
+
+ISR(TIMER1_COMPA_vect)
+{
+	TIMER_SOFTWARE_ModX();
+}
+
+void TIMER_Init()
+{
+	OCR1AH = (TIMER_VALUE >> 8) & 0xFF;
+	OCR1AL = (TIMER_VALUE & 0xFF);
+	TCCR1A = 0;	
+	TCCR1B = (1 << WGM12) | (1 << CS11);	
+	TIMSK = 1 << OCIE1A;
+}
---
+++ examples/avr-example-atmega16/avr-example-atmega16/timer.h
@@ -0,0 +1,16 @@
+/*
+ * timer.h
+ *
+ * Created: 06.06.2023 08:43:02
+ *  Author: valentin.stangaciu
+ */ 
+
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#define TIMER_VALUE 1843
+
+void TIMER_Init(void);
+
+#endif /* TIMER_H_ */
